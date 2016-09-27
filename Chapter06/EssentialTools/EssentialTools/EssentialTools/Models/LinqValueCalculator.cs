a7cb71cfc37369b8522a9de4dffc3aeba23eba54
Feifei GU <gufeifei@outlook.com>
2016-09-27 19:07:08 +0800

    adding IDiscountHelper

--- Chapter06/EssentialTools/EssentialTools/EssentialTools/Models/LinqValueCalculator.cs
+++ Chapter06/EssentialTools/EssentialTools/EssentialTools/Models/LinqValueCalculator.cs
@@ -7,9 +7,15 @@ namespace EssentialTools.Models
 {
     public class LinqValueCalculator: IValueCalculator
     {
+        private IDiscountHelper discounter;
+
+        public LinqValueCalculator(IDiscountHelper discountParam)
+        {
+            discounter = discountParam;
+        }
         public decimal ValueProducts(IEnumerable<Product> products)
         {
-            return products.Sum(p => p.Price);
+            return discounter.ApplyDiscount(products.Sum(p => p.Price));
         }
     }
 }
