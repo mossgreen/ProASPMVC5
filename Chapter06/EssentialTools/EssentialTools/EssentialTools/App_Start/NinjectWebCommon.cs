03116e28a4a16d0b3848fc7d750a9fffafa4a6c8
Feifei GU <gufeifei@outlook.com>
2016-09-27 18:58:08 +0800

    register the dependency resolver

--- Chapter06/EssentialTools/EssentialTools/EssentialTools/App_Start/NinjectWebCommon.cs
+++ Chapter06/EssentialTools/EssentialTools/EssentialTools/App_Start/NinjectWebCommon.cs
@@ -53,6 +53,7 @@ namespace EssentialTools.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new EssentialTools.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
