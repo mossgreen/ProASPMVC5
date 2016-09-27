af8a0f5bcc59a45dd3e98b2d6740512da5972078
Feifei GU <gufeifei@outlook.com>
2016-09-27 19:01:02 +0800

    refactoring the home controller, can run

--- Chapter06/EssentialTools/EssentialTools/EssentialTools/Controllers/HomeController.cs
+++ Chapter06/EssentialTools/EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator calc;
         private Product[] products =
         {
             new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
@@ -17,6 +18,11 @@ namespace EssentialTools.Controllers
             new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
             new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
         };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
         // GET: Home
         public ActionResult Index()
         {
