d2c69ce06e05369d4ab8a25a68ac2f0db4d88c51
Feifei GU <gufeifei@outlook.com>
2016-09-27 18:21:52 +0800

    adding classes: LinqValueCalculator, ShoppingCart

---
+++ Chapter06/EssentialTools/EssentialTools/EssentialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price);
+        }
+    }
+}
---
+++ Chapter06/EssentialTools/EssentialTools/EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calc;
+
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+       
+    }
+}
