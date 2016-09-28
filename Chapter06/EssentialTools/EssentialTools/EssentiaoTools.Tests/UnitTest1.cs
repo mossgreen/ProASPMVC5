80c57edccca7bb983b89a6f81db2ec427af104b3
Feifei GU <gufeifei@outlook.com>
2016-09-28 22:41:30 +0800

    unittest Discount_Between_10_And_100() run

--- Chapter06/EssentialTools/EssentialTools/EssentiaoTools.Tests/UnitTest1.cs
+++ Chapter06/EssentialTools/EssentialTools/EssentiaoTools.Tests/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace EssentiaoTools.Tests
     [TestClass]
     public class UnitTest1
     {
+        /*creates an instance of the object i'm going to test*/
         private IDiscountHelper getTestObject()
         {
             return new MinimumDiscountHelper();
@@ -23,6 +24,26 @@ namespace EssentiaoTools.Tests
 
             //assert
             Assert.AreEqual(total * 0.9M, discountedTotal);
+        }
+
+        [TestMethod]
+        public void Discount_Between_10_And_100()
+        {
+            //arrange
+            IDiscountHelper target = getTestObject();
+
+            //act 
+            decimal TenDollarDiscount = target.ApplyDiscount(10);
+            decimal HundredDollarDiscount = target.ApplyDiscount(100);
+            decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+
+            //assert
+            Assert.AreEqual(5, TenDollarDiscount, "$10 discount is wrong");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 discount is wrong");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 discount is wrong");
+            ;
+
+
         }
     }
 }
