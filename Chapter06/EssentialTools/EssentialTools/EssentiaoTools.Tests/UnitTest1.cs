9d6fd77dfefa75c2b3d6592d0c290c41c38a7008
Feifei GU <gufeifei@outlook.com>
2016-09-28 22:34:16 +0800

    adding first unittest

--- Chapter06/EssentialTools/EssentialTools/EssentiaoTools.Tests/UnitTest1.cs
+++ Chapter06/EssentialTools/EssentialTools/EssentiaoTools.Tests/UnitTest1.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
 
 namespace EssentiaoTools.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private IDiscountHelper getTestObject()
+        {
+            return new MinimumDiscountHelper();
+        }
         [TestMethod]
         public void TestMethod1()
         {
+            //arrange
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+
+            //act 
+            var discountedTotal = target.ApplyDiscount(total);
+
+            //assert
+            Assert.AreEqual(total * 0.9M, discountedTotal);
         }
     }
 }
