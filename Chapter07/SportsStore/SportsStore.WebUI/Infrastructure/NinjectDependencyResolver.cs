872e29f25c8505a7ed5c3fec8af4512624249000
Feifei GU <gufeifei@outlook.com>
2016-10-01 23:46:07 +0800

    making a mock repository

--- Chapter07/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ Chapter07/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -27,9 +30,22 @@ namespace SportsStore.WebUI.Infrastructure
             return kernel.GetAll(serviceType);
         }
 
+        //put bindings here
         private void AddBindings()
         {
-            //put bindings here
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football", Price = 25},
+                new Product {Name = "Surf board", Price = 179},
+                new Product {Name = "Running shoes", Price = 95}
+            });
+
+            /*I want Ninject return same mock object whenever 
+             *it gets a request for an implementation of IProductRepository interface.
+             Rather than create a new instance of object, it will satisfy requests for 
+             the IRepository interface with same mock obj*/
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
