63b30b494a431fbaa1a60b0cd301a1e28a0d6d77
Feifei GU <gufeifei@outlook.com>
2016-10-01 23:35:56 +0800

    adding interface IProductRepository
    using repository pattern

---
+++ Chapter07/SportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract
+{
+    /*this interface uses IEnumerable<T> to allow a caller 
+     * to obtain a sequesnce of Product objects,
+     without saying how or where the data is stored or retrieved.*/
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
