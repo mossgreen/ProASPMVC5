eabb2812eda3a865bef75f9182324f49d22d517b
Feifei GU <gufeifei@outlook.com>
2016-10-02 00:05:39 +0800

    modify default ReouterConfig

--- Chapter07/SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
+++ Chapter07/SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace SportsStore.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                /*name should be "ProductController", omit the "controller"*/
+                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
             );
         }
     }
