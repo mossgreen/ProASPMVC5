a3bc7e3737ae14b25f3093818d96a1514e34a187
Feifei GU <gufeifei@outlook.com>
2016-10-01 23:53:51 +0800

    adding a controller, ProductController

---
+++ Chapter07/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
