84a75a4a735a0ac939344bc9da2225091242fb69
Feifei GU <gufeifei@outlook.com>
2016-10-02 18:39:32 +0800

    got an error

--- Chapter07/SportsStore/SportsStore.Domain/Concrete/EFDBContext.cs
+++ Chapter07/SportsStore/SportsStore.Domain/Concrete/EFDBContext.cs
@@ -8,7 +8,7 @@ using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Concrete
 {
-    public class EFDBContext: DbContext
+    public class EFDbContext: DbContext
     {
         public DbSet<Product> Products { get; set; }
     }
--- Chapter07/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ Chapter07/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
+
     public class NinjectDependencyResolver : IDependencyResolver
     {
         private IKernel kernel;
@@ -30,22 +31,9 @@ namespace SportsStore.WebUI.Infrastructure
             return kernel.GetAll(serviceType);
         }
 
-        //put bindings here
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product {Name = "Football", Price = 25},
-                new Product {Name = "Surf board", Price = 179},
-                new Product {Name = "Running shoes", Price = 95}
-            });
-
-            /*I want Ninject return same mock object whenever 
-             *it gets a request for an implementation of IProductRepository interface.
-             Rather than create a new instance of object, it will satisfy requests for 
-             the IRepository interface with same mock obj*/
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
