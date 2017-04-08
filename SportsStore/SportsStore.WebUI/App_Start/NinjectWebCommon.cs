e3a4b48d574538727d87b996d0d858f1f31284d3
MossGU <gufeifei@outlook.com>
2017-04-08 23:03:59 +1200

    update gitIgnore to VS version

--- SportsStore/SportsStore.WebUI/App_Start/NinjectWebCommon.cs
+++ SportsStore/SportsStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -53,6 +53,15 @@ namespace SportsStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System
+                .Web
+                .Mvc
+                .DependencyResolver
+                .SetResolver(new 
+                    SportsStore
+                    .WebUI
+                    .Infrastructure
+                    .NinjectDependencyResolver(kernel));
         }        
     }
 }
