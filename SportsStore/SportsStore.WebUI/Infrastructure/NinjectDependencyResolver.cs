2a0de25c850f9be41fd3b10a16579abbeba4ad3c
MossGU <gufeifei@outlook.com>
2017-04-08 23:00:02 +1200

    add DI Container

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
