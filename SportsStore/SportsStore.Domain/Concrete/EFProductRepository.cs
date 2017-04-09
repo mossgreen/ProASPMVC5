12103ce438629907d0526c26434553c1adf83fe8
MossGU <gufeifei@outlook.com>
2017-04-09 15:56:08 +1200

    implement EF code first and DI container

---
+++ SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+
+        private EFDbContext context = new EFDbContext();
+        public IEnumerable<Product> Products { get { return context.Products; } }
+    }
+}
--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
 using Moq;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -20,17 +21,20 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            // -> this is how we mock data  <-
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
 
-            mock.Setup(m => 
-                m.Products)
-                .Returns(new 
-                    List<Product> { new Product { Name = "Football", Price = 25 }, new Product { Name = "Surf board", Price = 179 }, new Product { Name = "Running shoes", Price = 95 } });
+            //mock.Setup(m => 
+            //    m.Products)
+            //    .Returns(new 
+            //        List<Product> { new Product { Name = "Football", Price = 25 }, new Product { Name = "Surf board", Price = 179 }, new Product { Name = "Running shoes", Price = 95 } });
 
-            /*Rather than create a new instance of the implementation object each time, 
-             * Ninject will always satisfy requests for the IProductRepository interface 
-             * with the same mock object.*/
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            ///*Rather than create a new instance of the implementation object each time, 
+            // * Ninject will always satisfy requests for the IProductRepository interface 
+            // * with the same mock object.*/
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
 
         public object GetService(Type serviceType)
