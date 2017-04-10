559918f9e439393c44a227bc003dce7fad9ebf30
MossGU <gufeifei@outlook.com>
2017-04-10 20:52:14 +1200

    update assembly package and add unit test for pagination

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SportsStore.UnitTests
 {
@@ -9,6 +15,31 @@ namespace SportsStore.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products)
+                .Returns(new Product[] {
+                    new Product { ProductID = 1, Name = "P1" },
+                    new Product { ProductID = 2, Name = "P2" },
+                    new Product { ProductID = 3, Name = "P3" },
+                    new Product { ProductID = 4, Name = "P4" },
+                    new Product { ProductID = 5, Name = "P5" } });
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            //assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
         }
     }
 }
